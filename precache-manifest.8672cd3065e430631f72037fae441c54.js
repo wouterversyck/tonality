14bc30e47e79814c800c7ee9ac082f5555951f75
wouterversyck <wouter.versyck@gmail.com>
2020-09-08 20:02:03 +0200

    Deploy

---
+++ precache-manifest.8672cd3065e430631f72037fae441c54.js
@@ -0,0 +1,422 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8badb8ff01ebcdb7638",
+    "url": "/tonality/css/about.43debe42.css"
+  },
+  {
+    "revision": "dd38e8da15c88111e14f",
+    "url": "/tonality/css/app.9b6cd28a.css"
+  },
+  {
+    "revision": "4f514ee26072e9e0e01c",
+    "url": "/tonality/css/chunk-vendors.19e30246.css"
+  },
+  {
+    "revision": "282c5fdb1a70bd789e8aacf4986826da",
+    "url": "/tonality/img/chords/a/a-0.png"
+  },
+  {
+    "revision": "25d68d43c76ffd5f6906e98444949f3c",
+    "url": "/tonality/img/chords/a/a-1.png"
+  },
+  {
+    "revision": "c3dbe2a2250a3f099473e7589ee028bd",
+    "url": "/tonality/img/chords/a/a-2.png"
+  },
+  {
+    "revision": "43bc89113bd309dbfea66b03c595200a",
+    "url": "/tonality/img/chords/a/a-3.png"
+  },
+  {
+    "revision": "c3be454b119820a35f9df00892173951",
+    "url": "/tonality/img/chords/a/a-4.png"
+  },
+  {
+    "revision": "c979aa17112fdfd875dda26faedc0827",
+    "url": "/tonality/img/chords/a/a-5.png"
+  },
+  {
+    "revision": "20ee29693bb44b805c38a61d108e199e",
+    "url": "/tonality/img/chords/a/a-6.png"
+  },
+  {
+    "revision": "387af55a081aade095414250d07d436a",
+    "url": "/tonality/img/chords/a/a-7.png"
+  },
+  {
+    "revision": "4cd14e58aaf50b03492b44d526b160c7",
+    "url": "/tonality/img/chords/a_11/a_11-0.png"
+  },
+  {
+    "revision": "12e589ec4e02a30d0b7144485e6da07d",
+    "url": "/tonality/img/chords/a_11/a_11-1.png"
+  },
+  {
+    "revision": "b0f68695e2934c77cedb772ca3103290",
+    "url": "/tonality/img/chords/a_11/a_11-10.png"
+  },
+  {
+    "revision": "d42001a91eccb6aa167a7fa5a05b82ea",
+    "url": "/tonality/img/chords/a_11/a_11-11.png"
+  },
+  {
+    "revision": "c75c3e0cb601e335727fed69a37e0988",
+    "url": "/tonality/img/chords/a_11/a_11-12.png"
+  },
+  {
+    "revision": "d030e83c82b2da7678dcb6c99b292ba4",
+    "url": "/tonality/img/chords/a_11/a_11-13.png"
+  },
+  {
+    "revision": "7f95cedacc7fe5abdd70b3217924f7d1",
+    "url": "/tonality/img/chords/a_11/a_11-14.png"
+  },
+  {
+    "revision": "9f852e2f7d8c545eae7c8b8707b27275",
+    "url": "/tonality/img/chords/a_11/a_11-15.png"
+  },
+  {
+    "revision": "0de7b9b9a56f8910d5e90a47f568ef02",
+    "url": "/tonality/img/chords/a_11/a_11-16.png"
+  },
+  {
+    "revision": "fbce163a850e5a706006a551c3ea9cef",
+    "url": "/tonality/img/chords/a_11/a_11-2.png"
+  },
+  {
+    "revision": "eda4ec1fbbdfa5a26e047bb9806a005b",
+    "url": "/tonality/img/chords/a_11/a_11-3.png"
+  },
+  {
+    "revision": "19fc3fa06dba7bf55471692922918b0f",
+    "url": "/tonality/img/chords/a_11/a_11-4.png"
+  },
+  {
+    "revision": "fe2b52239bd8a47b870bea4364ba0d1e",
+    "url": "/tonality/img/chords/a_11/a_11-5.png"
+  },
+  {
+    "revision": "369b2dd6148792909ef8b405a043730d",
+    "url": "/tonality/img/chords/a_11/a_11-6.png"
+  },
+  {
+    "revision": "c9845237e8af76f7fa0431c2ac41abe1",
+    "url": "/tonality/img/chords/a_11/a_11-7.png"
+  },
+  {
+    "revision": "048023a40f7ee046f159a7c3a3704221",
+    "url": "/tonality/img/chords/a_11/a_11-8.png"
+  },
+  {
+    "revision": "45ad067731ed23aabb7eda1fdd2fbdde",
+    "url": "/tonality/img/chords/a_11/a_11-9.png"
+  },
+  {
+    "revision": "5910d75cd6739b601fb67162b2ef2215",
+    "url": "/tonality/img/chords/a_13/a_13-0.png"
+  },
+  {
+    "revision": "0690d80e77034911bb715ab0fe63425f",
+    "url": "/tonality/img/chords/a_13/a_13-1.png"
+  },
+  {
+    "revision": "aeb125579b4429bf4dcee63d8890a086",
+    "url": "/tonality/img/chords/a_13/a_13-10.png"
+  },
+  {
+    "revision": "254736a4806ad3b1df1fe1293d2d233d",
+    "url": "/tonality/img/chords/a_13/a_13-11.png"
+  },
+  {
+    "revision": "cbab56a94887662f5b0195a6632ca5e8",
+    "url": "/tonality/img/chords/a_13/a_13-12.png"
+  },
+  {
+    "revision": "f1b8cb7d35332c9824abc0fff64a9db6",
+    "url": "/tonality/img/chords/a_13/a_13-13.png"
+  },
+  {
+    "revision": "c37c9e731db121d55c5866e491254cfb",
+    "url": "/tonality/img/chords/a_13/a_13-14.png"
+  },
+  {
+    "revision": "052f6759ba40f398135a1ae8ca76fc77",
+    "url": "/tonality/img/chords/a_4/a_4-0.png"
+  },
+  {
+    "revision": "11fbb827e0e8f40acb4d7e8110795dda",
+    "url": "/tonality/img/chords/a_4/a_4-1.png"
+  },
+  {
+    "revision": "36365ed8c37210361fb660e44610aaaa",
+    "url": "/tonality/img/chords/a_4/a_4-2.png"
+  },
+  {
+    "revision": "149a4860808cc8803ac2170ca33251e3",
+    "url": "/tonality/img/chords/a_4/a_4-3.png"
+  },
+  {
+    "revision": "e46e63b22703747b32c83b42ae4c18e5",
+    "url": "/tonality/img/chords/a_4/a_4-4.png"
+  },
+  {
+    "revision": "692ca7ae9549a365cad0a4c4a14449ed",
+    "url": "/tonality/img/chords/a_4/a_4-5.png"
+  },
+  {
+    "revision": "d64858085b16339a30357bde57d01ce3",
+    "url": "/tonality/img/chords/a_4/a_4-6.png"
+  },
+  {
+    "revision": "210246a3adfc84b406530d03deba319f",
+    "url": "/tonality/img/chords/a_6/a_6-0.png"
+  },
+  {
+    "revision": "b71a4d1444554e53400dcb913cb2659e",
+    "url": "/tonality/img/chords/a_6/a_6-1.png"
+  },
+  {
+    "revision": "38aecdc27f25e695934df3b9f2d8821a",
+    "url": "/tonality/img/chords/a_6/a_6-2.png"
+  },
+  {
+    "revision": "393a1e7ae0fc0102862933ffb0a3b161",
+    "url": "/tonality/img/chords/a_6/a_6-3.png"
+  },
+  {
+    "revision": "1b22c5fb1fc1aa612fef96f47fdc661f",
+    "url": "/tonality/img/chords/a_6/a_6-4.png"
+  },
+  {
+    "revision": "a47c2a3436ad23be726d55426f4d79bf",
+    "url": "/tonality/img/chords/a_6/a_6-5.png"
+  },
+  {
+    "revision": "d9004bdba562d8777346a68a1bb4886f",
+    "url": "/tonality/img/chords/a_6/a_6-6.png"
+  },
+  {
+    "revision": "b97d2fd9690bb46f72be767f2e7bdac0",
+    "url": "/tonality/img/chords/a_7/a_7-0.png"
+  },
+  {
+    "revision": "d1cd67279f732b7707d7c6947bdfac0e",
+    "url": "/tonality/img/chords/a_7/a_7-1.png"
+  },
+  {
+    "revision": "2a4c047ffe9e5bc7257dfbe7488780da",
+    "url": "/tonality/img/chords/a_7/a_7-2.png"
+  },
+  {
+    "revision": "20864ec102ae2a07ac95f537e41eab7d",
+    "url": "/tonality/img/chords/a_7/a_7-3.png"
+  },
+  {
+    "revision": "3ae63ae28b9440cbf45d1a1a4727f975",
+    "url": "/tonality/img/chords/a_7/a_7-4.png"
+  },
+  {
+    "revision": "0f0af3f30a0b7a0fef5ccab1b395fe62",
+    "url": "/tonality/img/chords/a_7/a_7-5.png"
+  },
+  {
+    "revision": "1b18044f8018f96390eb2af25394d519",
+    "url": "/tonality/img/chords/a_7/a_7-6.png"
+  },
+  {
+    "revision": "917aacd60c86e134ba0abbc1998a7fa7",
+    "url": "/tonality/img/chords/a_7/a_7-7.png"
+  },
+  {
+    "revision": "7cc23449bfe2b3f11885451ce031fd2c",
+    "url": "/tonality/img/chords/a_7/a_7-8.png"
+  },
+  {
+    "revision": "3b2509a88aaa628ed16ea0b06abfed5b",
+    "url": "/tonality/img/chords/a_7_plus/a_7_plus-0.png"
+  },
+  {
+    "revision": "5e1124eece4c67d9b700426b9a77b9ed",
+    "url": "/tonality/img/chords/a_7_plus/a_7_plus-1.png"
+  },
+  {
+    "revision": "793581b81d5961d532f6856f69067da3",
+    "url": "/tonality/img/chords/a_7_plus/a_7_plus-2.png"
+  },
+  {
+    "revision": "1016eae2b6fe193b53910f29b291f606",
+    "url": "/tonality/img/chords/a_7_plus/a_7_plus-3.png"
+  },
+  {
+    "revision": "24433651752b6e2eeb0bf6f2c5c3c639",
+    "url": "/tonality/img/chords/a_7_plus/a_7_plus-4.png"
+  },
+  {
+    "revision": "d80a4c0b0153c8959a95f9b72193afe4",
+    "url": "/tonality/img/chords/a_7_plus/a_7_plus-5.png"
+  },
+  {
+    "revision": "d4a87023514952afd8fdd8d9372531b0",
+    "url": "/tonality/img/chords/a_7_plus/a_7_plus-6.png"
+  },
+  {
+    "revision": "111eb4d05cf06981e8f803734e0e982d",
+    "url": "/tonality/img/chords/a_7_plus/a_7_plus-7.png"
+  },
+  {
+    "revision": "99a22100a3cc184670281122522e400a",
+    "url": "/tonality/img/chords/a_7_plus/a_7_plus-8.png"
+  },
+  {
+    "revision": "4139e19eaeb778b53fc77dfa5aaf18d5",
+    "url": "/tonality/img/chords/a_plus/a_plus-0.png"
+  },
+  {
+    "revision": "f56719d76a2c055be1b58b8cf4532a9d",
+    "url": "/tonality/img/chords/a_plus/a_plus-1.png"
+  },
+  {
+    "revision": "13f10c96fcfcddfd9073cf988286ac1e",
+    "url": "/tonality/img/chords/a_sharp/a_sharp-0.png"
+  },
+  {
+    "revision": "3ed812f71eedba96c34cd9b2555d8b08",
+    "url": "/tonality/img/chords/a_sharp/a_sharp-1.png"
+  },
+  {
+    "revision": "864e9de7f2453e7309c030a85911d878",
+    "url": "/tonality/img/chords/a_sharp/a_sharp-2.png"
+  },
+  {
+    "revision": "73200419e7b9552be3209075adae0656",
+    "url": "/tonality/img/chords/a_sharp/a_sharp-3.png"
+  },
+  {
+    "revision": "e5aba5fd674dcb61ddbad500c55edf8b",
+    "url": "/tonality/img/chords/a_sharp/a_sharp-4.png"
+  },
+  {
+    "revision": "83ffb215ca2b613ac98f9f7158d414a0",
+    "url": "/tonality/img/chords/a_sharp/a_sharp-5.png"
+  },
+  {
+    "revision": "91d48ed454addd335efe1c43e91300a2",
+    "url": "/tonality/img/chords/a_sharp_7/a_sharp_7-0.png"
+  },
+  {
+    "revision": "8d900201296d7d65497bfdb38ce45628",
+    "url": "/tonality/img/chords/a_sharp_7/a_sharp_7-1.png"
+  },
+  {
+    "revision": "9c2934a4be1719761d44f4c5ff741176",
+    "url": "/tonality/img/chords/a_sharp_7/a_sharp_7-2.png"
+  },
+  {
+    "revision": "998efb8df048072b5fd502a500888b58",
+    "url": "/tonality/img/chords/a_sharp_7/a_sharp_7-3.png"
+  },
+  {
+    "revision": "129e51a2e5129193cba2d9ef9afeade8",
+    "url": "/tonality/img/chords/a_sharp_7/a_sharp_7-4.png"
+  },
+  {
+    "revision": "fd013125a336befe6c98b325bdcbb2df",
+    "url": "/tonality/img/chords/a_sharp_7/a_sharp_7-5.png"
+  },
+  {
+    "revision": "ef8ab7140ab17764facaf5920d53d127",
+    "url": "/tonality/img/chords/a_sharp_dim/a_sharp_dim-0.png"
+  },
+  {
+    "revision": "38e7ef5bebbef34d1a61e75b8c283d7c",
+    "url": "/tonality/img/chords/a_sharp_dim/a_sharp_dim-1.png"
+  },
+  {
+    "revision": "1e3448931513f01517d1b449a680f8c6",
+    "url": "/tonality/img/chords/a_sharp_dim/a_sharp_dim-2.png"
+  },
+  {
+    "revision": "35fcfcb4dace0f2021d4328d39432d04",
+    "url": "/tonality/img/chords/a_sharp_m/a_sharp_m-0.png"
+  },
+  {
+    "revision": "a72dfe0dc1dc1804e16d6709dc500a62",
+    "url": "/tonality/img/chords/a_sharp_m/a_sharp_m-1.png"
+  },
+  {
+    "revision": "c1a15fdd8855bfbe5bacae5c8cf7cf81",
+    "url": "/tonality/img/chords/a_sharp_m/a_sharp_m-2.png"
+  },
+  {
+    "revision": "565fe688de754701e3b1490c58a925f9",
+    "url": "/tonality/img/chords/a_sharp_m_7/a_sharp_m_7-0.png"
+  },
+  {
+    "revision": "aaea7ac49b94dd7ddd00a811ffa52582",
+    "url": "/tonality/img/chords/a_sharp_m_7/a_sharp_m_7-1.png"
+  },
+  {
+    "revision": "a26de816d4fd04b489bcbc5769ff870f",
+    "url": "/tonality/img/chords/a_sharp_m_7/a_sharp_m_7-2.png"
+  },
+  {
+    "revision": "306cffdd4f5f759fe11825452de2f856",
+    "url": "/tonality/img/chords/a_sharp_m_7/a_sharp_m_7-3.png"
+  },
+  {
+    "revision": "2b7488ed141864943b8d44a5ff9a5e17",
+    "url": "/tonality/img/chords/a_sharp_m_7/a_sharp_m_7-4.png"
+  },
+  {
+    "revision": "a54909b66a20293d48490e81b6a3a410",
+    "url": "/tonality/img/chords/a_sharp_maj_7/a_sharp_maj_7-0.png"
+  },
+  {
+    "revision": "791e927f2418efa5160f8566b75ad82e",
+    "url": "/tonality/img/chords/a_sharp_maj_7/a_sharp_maj_7-1.png"
+  },
+  {
+    "revision": "4aed54aadb28b9875a5c9d22ba1bc4d9",
+    "url": "/tonality/img/chords/a_sharp_maj_7/a_sharp_maj_7-2.png"
+  },
+  {
+    "revision": "8ba0626d89f407419467c1d4cc25b0b2",
+    "url": "/tonality/img/chords/a_sharp_maj_7/a_sharp_maj_7-3.png"
+  },
+  {
+    "revision": "a8904bc2bea24839b1caf78c2b1d46cd",
+    "url": "/tonality/img/chords/a_sharp_maj_7/a_sharp_maj_7-4.png"
+  },
+  {
+    "revision": "05535f2d2d0c1abf9bc8c3686f2eb76e",
+    "url": "/tonality/img/chords/a_sharp_maj_7/a_sharp_maj_7-5.png"
+  },
+  {
+    "revision": "042db295941cbffa631690b9d758a7be",
+    "url": "/tonality/img/chords/a_sharp_maj_7/a_sharp_maj_7-6.png"
+  },
+  {
+    "revision": "1c5d7dbbb7d3522376888aedaa4121ed",
+    "url": "/tonality/index.html"
+  },
+  {
+    "revision": "e8badb8ff01ebcdb7638",
+    "url": "/tonality/js/about.d13cf226.js"
+  },
+  {
+    "revision": "dd38e8da15c88111e14f",
+    "url": "/tonality/js/app.1dfe0b26.js"
+  },
+  {
+    "revision": "4f514ee26072e9e0e01c",
+    "url": "/tonality/js/chunk-vendors.ba8034e3.js"
+  },
+  {
+    "revision": "920b5d4fc85f418ebd265ccede8ea69c",
+    "url": "/tonality/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tonality/robots.txt"
+  }
+]);
